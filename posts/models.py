ede2c2ecccadf774e3e9e55c3f6bc57d0b9c09d0
Jasmine Green-Key <greinkejs1069@marybaldwin.edu>
2022-03-24 19:54:34 -0500

    first commit

---
+++ posts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls import reverse
+
+class Post (models.Model): 
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+
+    def __str__(self): 
+        return self.title
+
+    def get_absolute_url(self): 
+        return reverse('post_detail', args=[self.id])
+            # args is short for arguments. reverse will take the information submitted into the 
+            # form and return to the post_detail url.  
